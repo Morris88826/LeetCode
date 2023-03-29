736af8a8c40eecbea3af55201657156f3ea7f6c0
morris88826 <morris88826@gmail.com>
2023-03-30 01:31:13 +0800

    20230329

--- Solution/0028.cpp
+++ Solution/0028.cpp
@@ -18,11 +18,63 @@ Constraints:
 1 <= haystack.length, needle.length <= 104
 haystack and needle consist of only lowercase English characters.
 
+Hint: Review Rabin-Karp Algorithm
 
 '''
 
 class Solution {
 public:
+
+    int hashValue(string value, int RADIX, int MOD, int m){
+        long out = 0;
+        for(int i=0; i<m; i++){
+            out = ((out*RADIX)%MOD + (int)(value[i]-'a'))%MOD;
+        }
+        return (int)out;
+    }
+    int strStr(string haystack, string needle) { // Rabin-Karp Algorithm
+        
+        if(haystack.length()<needle.length()){
+            return -1;
+        }
+
+        int RADIX = 26;
+        int MOD = 1000000033;
+        int m = needle.length();
+
+        int target = hashValue(needle, RADIX, MOD, needle.length());
+        long _target = 0;
+
+        long MAX_WEIGHT = 1;
+        for(int i=0; i<needle.length(); i++){
+            MAX_WEIGHT = (MAX_WEIGHT*RADIX)%MOD;
+        }
+
+        for(int i=0; i<haystack.length()-needle.length()+1; i++){
+            if(i==0){
+                _target = hashValue(haystack, RADIX, MOD, m);
+            }else{
+                _target = ((_target*RADIX)%MOD - ((int)(haystack[i-1]-'a')*MAX_WEIGHT)%MOD + (int)(haystack[i+m-1]-'a')+MOD)%MOD; //prevent from having negative number
+            }
+
+            bool same = true;
+            if(_target==target){
+                for(int j=0; j<m; j++){
+                    if(haystack[i+j]!=needle[j]){
+                        same = false;
+                        break;
+                    }
+                }
+                if(same){
+                    return i;
+                }
+            }
+            
+        }
+        return -1;
+    }
+
+
     int strStr_v1(string haystack, string needle) { // Naive approach
 
         if(haystack.length()<needle.length()){
@@ -44,5 +96,9 @@ public:
             }
         }
         return -1;
+    }
+
+    int strStrPractice(string haystack, string needle) {
+        
     }
 };
